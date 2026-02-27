5aa09fc33022b4ecd3f670a253d5189bca938961
TheIsaac503 <jhilandaverde113@gmail.com>
2026-02-26 20:00:16 -0600

    Agrego una carpeta y un archivo de python

---
+++ Python/Suma.py
@@ -0,0 +1,47 @@
+# es un comentario
+
+## para declarar una variable
+nombre = "Ever"
+print(nombre)
+
+edad = 40
+
+print(nombre, edad, " years old")  ## concatenar
+# binario = 0
+# binario = 1
+# string = letras
+# objeto = 012345
+
+
+# inmutabilidad  :
+# pero siempre deben de conservar su tipo
+
+
+## direccion # 0x
+## tipo => numerico o string ( booleano, objeto)
+## tipo numero => perpara a la memoria
+## para guardar un tipo de dato string exa.
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 + numero2
+print("El resultado de la suma es:", resultado)
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 - numero2
+print("El resultado de la resta es:", resultado)
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 * numero2
+print("El resultado de la multiplicacion es:", resultado)
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 / numero2
+print("El resultado de la dividicion es:", resultado)
