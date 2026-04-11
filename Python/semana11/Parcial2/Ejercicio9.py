cc7439861d254b2ce52f82cf8dd10b5541d6af5f
TheIsaac503 <jhilandaverde113@gmail.com>
2026-04-11 10:49:51 -0600

    Ejercicio 9 del parcial 2, enfocado en remplazar algo del dato dado y convertilo a minusculas

---
+++ Python/semana11/Parcial2/Ejercicio9.py
@@ -0,0 +1,11 @@
+# Ejercicio 9
+# 1. Toma la cadena "Any time y Anytime".
+# 2. Reemplaza todas las apariciones de "Any time" (con espacio)
+# por "Always".
+# 3. Convierte todo el texto resultante a mayusculas.
+
+texto = "Any time y Anytime"
+nuevo = texto.replace("Any time", "Always")
+
+resultado = nuevo.upper()
+print(resultado)
