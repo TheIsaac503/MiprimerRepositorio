1052ca7b8b5f8f3264672cb2252122d7158e0071
TheIsaac503 <jhilandaverde113@gmail.com>
2026-04-11 10:36:45 -0600

    Ejercicio 5 del parcial 2, enfocado en invertir las mayusculas en minusculas y viceversa, llenarlo con ateriscos hasta un total de 15 espacios

--- Python/semana11/Parcial2/Ejercicio5.py
+++ Python/semana11/Parcial2/Ejercicio5.py
@@ -3,7 +3,7 @@
 # 2. Invierte las mayusculas por minusculas y las minusculas por mayusculas.
 # 3. Alinea este nuevo texto hacia la izquierda en un espacio de
 # 15 caracteres, rellenando los espacios vacios con asteriscos ("*").
-
+# r
 
 texto = "pYTHON"
 textoinvertido = texto.swapcase()
