702c40285bb073a6f099e78da2bc952c757a4fc6
TheIsaac503 <jhilandaverde113@gmail.com>
2026-04-11 10:23:19 -0600

    Ejercicio 2 del parcial 2, enfocado en en reemplazar un dato de una cadena y cambiarlo por otro

---
+++ Python/semana11/Parcial2/Ejercicio2.py
@@ -0,0 +1,12 @@
+# Ejercicio 2
+# 1. Toma la cadena de texto "Su nombre"".
+# 2. Convierte el texto para que la primera letra de cada
+# una de las palabras este en mayúscula.
+
+
+texto = "Isaac"
+
+textoCambiado = texto.title()
+textofinal = textoCambiado.replace("Isaac", "Gutierez")
+
+print(textofinal)
