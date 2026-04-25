28f2eabd3037485aaf04b03e2fd6c7aeb13a7ca6
TheIsaac503 <jhilandaverde113@gmail.com>
2026-04-25 12:10:11 -0600

    Ejercicio 4 de la semana 12, uso de if,for y while

---
+++ Python/semana12/Ejercicio4.py
@@ -0,0 +1,9 @@
+# Ejercicio 4:
+# Pide un número y determina si es par o impar.
+
+numero = int(input("Dime un numero: "))
+
+if numero % 2 == 0:
+    print("Este Numero es par")
+else:
+    print("Este numero es impar")
