d8770c202a17ac35397ffd2d78505e3ec5eadf59
TheIsaac503 <jhilandaverde113@gmail.com>
2026-04-25 12:11:36 -0600

    Ejercicio 9 de la semana 12, uso de if,for y while

---
+++ Python/semana12/Ejercicio9.py
@@ -0,0 +1,9 @@
+# Ejercicio 9:
+# Solicita un año y determina si es bisiesto.
+
+año = int(input("Ingresa un año: "))
+
+if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
+    print("Año bisiesto")
+else:
+    print("No es bisiesto")
