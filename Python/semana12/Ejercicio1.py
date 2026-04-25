1d07f03b1943f67ce92a805d2220ca7ce14f965f
TheIsaac503 <jhilandaverde113@gmail.com>
2026-04-25 12:09:12 -0600

    Ejercicio 1 de la semana 12, uso de if,for y while

---
+++ Python/semana12/Ejercicio1.py
@@ -0,0 +1,11 @@
+# Ejercicio 1:
+# Pide un número al usuario e indica si es positivo, negativo o cero usando if, elif y else.
+
+numero = int(input("Dime un numero: "))
+
+if numero > 0:
+    print("Este numero es positivo")
+elif numero < 0:
+    print("Este numero es negativo")
+else:
+    print("Este numero es 0")
